61f4355f28c7efeedd16ed16074b0e3b50a6dd0a
lagoke <olagokeamiola@gmail.com>
2023-01-07 11:51:07 -0500

    Prep 1 finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,29 @@
 using System;
 
-class Program
+namespace ask_and_show_names
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep1 World!");
+        /*
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+     
+       }
+
+*/
+
+        static void Main(string[] args)
+        {
+            Console.Write("What is your first name? ");
+            string first = Console.ReadLine();
+
+            Console.Write("What is your last name? ");
+            string last = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {last}, {first} {last}.");
+        }
+
+
     }
 }
