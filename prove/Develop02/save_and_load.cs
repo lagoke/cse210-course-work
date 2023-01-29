08e13cb690973531ce5ccf4203c0840420047a8c
lagoke <olagokeamiola@gmail.com>
2023-01-28 20:20:55 -0500

    Week4 Save changes

--- prove/Develop02/save_and_load.cs
+++ prove/Develop02/save_and_load.cs
@@ -7,22 +7,85 @@ namespace JournalApp
 {
     class Save_and_Load
     {
-        public void save_journal(string filename)
-        {
-            // Copy the contents of "myjournal.txt" to the specified file
-            File.Copy("myjournal.txt", filename, true);
-        }
 
-        public void load_journal(string filename_passed)
+        public void save_journal(string send_filename)
         {
-            // Read all lines from the specified file
-            string[] lines = System.IO.File.ReadAllLines(filename_passed);
+            string filename_passed = send_filename;
 
-            // Print each line to the console
+            /*
+            // First retrieve the content of the present file name
+            string filename = "myjornal.txt";
+            string[] lines = System.IO.File.ReadAllLines(filename);
+
             foreach (string line in lines)
             {
-                Console.WriteLine(line);
+                string[] parts = line.Split("\n");
+
+                string line_of_text1 = parts[0];
+                //string line_of_text2 = parts[1];
+
+                //Console.WriteLine(line_of_text1);
+                //Console.WriteLine(line_of_text2);
+
+
+                DateTime theCurrentTime = DateTime.Now;
+                string dateText = theCurrentTime.ToShortDateString();
+
+               // string fileName = "myjornal.txt";
+
+                //string text_to_store = "\n";
+
+                using (StreamWriter outputFile = new StreamWriter(filename_passed))
+                {
+                    // Add the question displayed to the file
+                    // outputFile.WriteLine(helpful_questions.ElementAt(_someRandomjournal_question));
+
+                    // Add the user's response to the displayed question inside the file
+
+
+
+                    //string my_new_text = outputFile.WriteLine($"Date:  { dateText} - Prompt: \n { user_response} ").ToString();
+                    //text_to_store += my_new_text;
+                    outputFile.WriteLine($" { line_of_text1}");
+
+                }
+
+
             }
+
+
+            */
+
+
+            File.Copy("myjornal.txt", "filename_passed", true);
+
+
         }
+
+
+
+        public void load_journal(string filename)
+        {
+
+            string filename_passed = filename;
+            string[] lines = System.IO.File.ReadAllLines(filename_passed);
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split("\n");
+
+                string line_of_text1 = parts[0];
+                //string line_of_text2 = parts[1];
+
+                Console.WriteLine(line_of_text1);
+                //Console.WriteLine(line_of_text2);
+            }
+
+
+        }
+
+
+
+
     }
-}
+    }
