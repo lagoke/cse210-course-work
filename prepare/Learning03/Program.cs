85632df7fb1a499f539d8c644a2a1c1b7f8f3a87
lagoke <olagokeamiola@gmail.com>
2023-02-01 13:42:46 -0500

    Prep 3 and 4 revisited

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,96 @@
-using System;
+//Console.WriteLine("Hello, World!");
 
-class Program
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning03 World!");
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            string welcome;
+            int num;
+            int tries;
+            bool playAgain = true;
+            var response = "";
+
+            while (playAgain)
+            {
+                var guess = 0;
+                tries = 0;
+                welcome = "Guess a number between 1 and 100";
+                num = rand.Next(1, 101);
+                Console.WriteLine(welcome);
+
+                response = "";
+
+                while (guess != num)
+                {
+                    Console.Write("Guess: ");
+                    var input = Console.ReadLine();
+                    while (!int.TryParse(input, out guess))
+                    {
+                        Console.WriteLine("That's not a number.");
+                        Console.WriteLine("Enter a number between 1 and 100");
+                        input = Console.ReadLine();
+                        if (int.TryParse(input, out guess))
+                        {
+                            Console.WriteLine($"{guess} is better");
+                            continue;
+                        }
+                    }
+
+                    if (guess > num)
+                    {
+                        Console.WriteLine("Lower");
+                        tries++;
+                        continue;
+                    }
+                    else if (guess < num)
+                    {
+                        Console.WriteLine("Higher");
+                        tries++;
+                        continue;
+                    }
+                    else if (guess == num)
+                    {
+                        Console.WriteLine("Correct");
+                        tries++;
+                        continue;
+                    }
+                }
+                Console.WriteLine("You guessed it! The number was {0}!", num);
+                //Console.WriteLine("Congrats, it took you " + tries + " tries");
+                Console.WriteLine("It took you {0} {1}.\n", tries, tries == 1 ? "try" : "tries");
+                //Console.ReadLine();
+
+                Console.WriteLine("Would you like to play again (Yes/No): ");
+                response = Console.ReadLine();
+                while (string.IsNullOrEmpty(response) || string.IsNullOrWhiteSpace(response))
+                {
+                    Console.WriteLine("Response can't be empty! Input your response once more");
+                    response = Console.ReadLine();
+                }
+                response = response.ToUpper();
+
+                if (response == "YES")
+                {
+                    playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
+                }
+
+            }
+            Console.WriteLine("Thanks for playing!");
+            //Console.ReadKey();
+        }
     }
 }
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,82 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
-class Program
+namespace ManipulateList
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning04 World!");
+        static void Main(string[] args)
+        {
+            //double number;
+            Console.WriteLine("Enter a list of numbers, type 0 when finished\n");
+            List<double> items = new List<double>();
+
+            string userInput;
+            //int list_length;
+            double sum;
+            double minPosNum;
+
+            // repeatedly ask for input from the user until it's a valid double
+            do
+            {
+                double newItem = 0.0;
+                minPosNum = 0.0;
+                Console.Write(string.Format("Enter number: "));
+                // read from console into userInput
+                userInput = Console.ReadLine();
+                // check if the user's input is a number
+                while (!double.TryParse(userInput, out newItem))
+                {
+                    Console.WriteLine("That's not a number.");
+                    Console.WriteLine("Enter a number");
+                    userInput = Console.ReadLine();
+                }
+
+                if (newItem > 0)
+                {
+                    if (newItem < minPosNum)
+                    {
+                        minPosNum = newItem;
+                    }
+                }
+                // add the new item to the array
+                items.Add(newItem);
+            } while (userInput != "0");
+
+            // output all the items to the console, separated by commas
+            if (items.Count > 0)
+            {
+                items.RemoveAt(items.Count - 1);
+            }
+            Console.Write("Your list elements are : \n");
+            Console.WriteLine(string.Join("\n ", items.Select(item => item.ToString())));
+
+            //Console.Write($"Number of elements: {list_length}\n");
+            Console.Write($"Number of elements: {items.Count}\n");
+
+            sum = items.Sum();
+            Console.Write($"Sum of elements: {sum}\n");
+
+            double avg = sum / items.Count;
+            Console.Write($"Average of elements: {avg}\n");
+
+            //To find the largest element in the list and its position
+            int maxValue = (int)items.Max();
+            int maxIndex = items.IndexOf(maxValue);
+            //To find the smallest element in the list and its position
+            int minValue = (int)items.Min();
+            int minIndex = items.IndexOf(minValue);
+            Console.WriteLine("Maximum element {0} present at index {1}", maxValue, maxIndex);
+            Console.WriteLine("Minimum element {0} present at index {1}", minValue, minIndex);
+
+            //The smallest positive number
+            Console.Write($"The smallest positive number is: {minPosNum}\n");
+
+            //Sorting
+            items.Sort();
+            Console.Write("After Sorting, Your list elements are : \n");
+            Console.WriteLine(string.Join("\n ", items.Select(item => item.ToString())));
+        }
     }
 }
